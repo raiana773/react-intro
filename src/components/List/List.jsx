b5c6d7c6e6df826eb2bd356669ed25acf14ab72d
raiana773 <104357057+raiana773@users.noreply.github.com>
2022-06-14 16:17:01 +0545

    props-jsx

---
+++ src/components/List/List.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Card from "../Card/Card";
+
+const List = ({ cars }) => {
+  console.log(cars);
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}>
+      {cars.map(item => (
+        <Card key={item.id} item={item} />
+        // <span key={item.id}>{item.model}</span>
+      ))}
+    </div>
+  );
+};
+
+export default List;
